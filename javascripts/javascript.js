77a5037b2dea0efb1ce4a2ed478310da75afa927
kunal-sin <kunal.singh@innoraft.com>
2018-02-09 18:15:52 +0530

    Changed

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -84,6 +84,15 @@ $(document).ready(function()
               $(".triangle-right").removeClass('triangle-down');
           }
 
+          if($(window).width()>767)
+          {
+              $(".column").removeClass('col-xs-offset-1');
+          }
+          else
+          {
+            $(".column").addClass('col-xs-offset-1');
+          }
+
           if($(window).width()<416)
           {
             $(".column").removeClass('col-xs-10');
@@ -100,13 +109,8 @@ $(document).ready(function()
             $(".column").addClass('col-xs-10');
             $(".column").removeClass('col-xs-12');
             $(".column").addClass('col-xs-10');
-            $(".column").addClass('col-xs-offset-1');
           }
 
-          if($(window).width()>767)
-          {
-            $(".column").removeClass('col-xs-offset-1');
-          }
 
         });
 
